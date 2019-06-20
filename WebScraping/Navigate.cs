c92544173a1bde396eec5e6cfd74a1ca55f8a809
synergize <err0r142@gmail.com>
2019-06-20 01:42:00 -0700

    Add project files.

---
+++ WebScraping/Navigate.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace WebScraping
+{
+    class Navigate
+    {
+        private static string _DriverDirectory = Path.Combine(Path.GetFullPath(Directory.GetCurrentDirectory()), "Driver");
+        
+        public void TestingDriver()
+        {
+            List<string> NewList = new List<string>();
+            var option = new ChromeOptions();
+            option.AddArgument("--headless");
+            IWebDriver driver = new ChromeDriver(BuildFilePath(), option);
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl("https://www.mtggoldfish.com/movers/paper/modern");
+            var Movers = driver.FindElements(By.XPath("//table[contains(@class, 'movers-table')]"));
+            foreach (var item in Movers)
+            {
+                NewList.Add(item.Text);
+                Console.WriteLine(item.Text);
+            }
+            driver.Close();
+            driver.Quit();
+
+            int z = 0;
+
+        }
+        public string BuildFilePath()
+        {
+            if (!Directory.Exists(_DriverDirectory))
+            {
+                Directory.CreateDirectory(_DriverDirectory);
+            }
+            return Path.Combine(_DriverDirectory);
+        }
+    }
+}
---
+++ WebScraping/Program.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+
+namespace WebScraping
+{
+    class Program
+    {
+        private static string _DriverDirectory = Path.Combine(Path.GetFullPath(Directory.GetCurrentDirectory()), "Driver");
+        static void Main(string[] args)
+        {
+            Navigate Test = new Navigate();
+            Test.TestingDriver();
+        }
+    }
+}
